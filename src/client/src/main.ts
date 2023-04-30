673ba0cbf9056f105e106c997cf798ea85e0f269
Gergely Szentesi <g.szentesi@hvg.hu>
2023-04-30 11:53:48 +0200

    ongoing

--- src/client/src/main.ts
+++ src/client/src/main.ts
@@ -1,12 +1,18 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import PrimeVue from 'primevue/config'
+import Button from 'primevue/button'
+
 import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
 
 const app = createApp(App)
+
+app.use(PrimeVue)
+app.component('Button', Button)
 
 app.use(createPinia())
 app.use(router)
